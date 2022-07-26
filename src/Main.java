8bdc14f5805fac58e940a359f25088fc630b81fc
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2022-07-26 09:12:17 -0400

    Ingredient search working
    Now splitting features into classes

--- src/Main.java
+++ src/Main.java
@@ -62,15 +62,15 @@ public class Main {
                 // Prompts user to enter string search term
                 System.out.println("Enter the first ingredient:");
                 //String ingredientOne = scan.nextLine();
-                String ingredientOne = "flour";
+                String ingredientOne = "cheese";
 
                 System.out.println("Enter the second ingredient:");
                 //String ingredientTwo = scan.nextLine();
-                String ingredientTwo = "sugar";
+                String ingredientTwo = "flour";
 
                 System.out.println("Enter the last ingredient:");
                 //String ingredientThree = scan.nextLine();
-                String ingredientThree = "oranges";
+                String ingredientThree = "tomatosauce";
 
                 // Creating Search Object that will return JSON String Body
 
@@ -89,82 +89,11 @@ public class Main {
                 scan.nextLine();
             }
 
-
         }
 
 
-
-        // Using Jackson Library to create a JSON Node object from the Json String
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode api = objectMapper.readTree(results);
-
-        // Create Random integer for picking random search result later in the program
-        Random randInt = new Random();
-        int max = 0;
-
-
-        System.out.println(api.size());
-
-//        // Prints only results that contain at least 1 item in the ingredient search
-//        for (JsonNode items : api) {
-//            if (items.get("usedIngredientCount").asInt() >= 1) {
-//                System.out.println(items);
-//            }
-//
-//        }
-
-        //System.out.println(api.get(0).get("usedIngredientCount"));
-
-        // Logic to correct a bug that will cause choosing a random index fail. The total amount of returned food
-        // items cannot be larger than 100 due to current limitations, even if the total amount in the database
-        // is more than 100. Example - Database has 400 items that return for "apple", but the Json will only contain
-        // a Maximum of 100. To avoid out of bounds index issues, setting up a conditional that will ensure we stay
-        // within the correct range.
-//        System.out.println(results);
-        if (api.size() > 100) {
-            max = 99;
-        } else {
-            max = api.size();
-        }
-
-        // Making JSON Object of just the Food Results , ie - Title="Pasta", ID="123456"
-        JsonNode recipeResults = api.get("results");
-        ArrayList <JsonNode> chosenRecipesId = new ArrayList<>();
-
-        // Initialize three empty HashMaps that will contain the info for chosen recipes
-        HashMap<String, String> foodOne = new HashMap<>();
-        HashMap<String, String> foodTwo = new HashMap<>();
-        HashMap<String, String> foodThree = new HashMap<>();
-
-        // Initialize an empty Array that will house the three HashMaps - then adds the HashMaps to the array
-        ArrayList <HashMap> chosenFoodList = new ArrayList<>();
-        chosenFoodList.add(foodOne);
-        chosenFoodList.add(foodTwo);
-        chosenFoodList.add(foodThree);
-
-        // Gets three random food ids from the list of search results
-        for (int i = 0; i < 3; i++) {
-            chosenRecipesId.add(recipeResults.get(randInt.nextInt(max)).get("id"));
-        }
-
-        // Loop that takes food ids from the Array chosenRecipesId, and gets the specific recipe information
-        // per id, then assigned the key / value pair to the Array of Hashmaps ( chosenFoodList ) so that
-        // we have 1 Array list, with 3 Hashmaps, that are the title, id, image & link for the specific
-        // randomly chosen recipe. We'll use this data structure to pull the info we need for the Webpage using the
-        // ".get" method on the Array Index for each Recipe.
-        for (int i = 0; i < 3; i++) {
-
-            HttpResponse<String> recipeResponse = food.recipeInfo(chosenRecipesId.get(i).asText());
-            JsonNode recipeJsonString = objectMapper.readTree(recipeResponse.body());
-            chosenFoodList.get(i).put("id", recipeJsonString.get("id"));
-            chosenFoodList.get(i).put("title", recipeJsonString.get("title"));
-            chosenFoodList.get(i).put("sourceUrl", recipeJsonString.get("sourceUrl"));
-            chosenFoodList.get(i).put("image", recipeJsonString.get("image"));
-
-        }
-
-        System.out.println(chosenFoodList);
-
+        // return value from json class
+        //System.out.println(chosenFoodList);
 
 
         // Current additional output - Credits Left, Total Search Results, Ex
