af2e3ea245b9cb6a8de112cd32d60f05ff3a9d3b
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2022-10-01 20:01:43 -0400

    refactoring input class - adding while loops to control incorrect input

--- src/Input.java
+++ src/Input.java
@@ -26,104 +26,114 @@ public class Input {
         validChoiceStr.add("y");
         validChoiceStr.add("n");
 
-        // Prompts user to choose search mode
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter 1 to search by title or 2 to search by ingredient:");
 
-        // Verifies valid choice was made by user. If valid moves to next step. If invalid, User reenters.
-        try {
-            userInput = scan.nextInt();
+        boolean isModeSet = false;
+        while (!isModeSet) {
 
-        // Moves the scanner forward to account for the new-line character after using nextInt()
-            scan.nextLine();
-        } catch (Exception notInt) {
-            System.out.println("Please enter a valid choice...");
-            term();
+            // Prompts user to choose search mode
+            System.out.println("Enter 1 to search by title or 2 to search by ingredient:");
+
+            // Verifies valid choice was made by user. If valid moves to next step. If invalid, User reenters.
+            try {
+                userInput = scan.nextInt();
+
+            // Moves the scanner forward to account for the new-line character after using nextInt()
+                scan.nextLine();
+            } catch (Exception notInt) {
+                System.out.println("Please enter a valid choice...");
+                continue;
+            }
+            if (validChoiceInt.contains(userInput) != true) {
+                System.out.println("Please enter a valid choice...");
+                continue;
+            }else{
+                isModeSet = true;
+            }
+
         }
-        if (validChoiceInt.contains(userInput) != true) {
-            System.out.println("Please enter a valid choice...");
-            term();
-        }
-
 
         String results = null;
 
-        while (true) {
+        if (userInput == 1) {
 
-            if (userInput == 1) {
+            // Prompts user to enter string search term
+            System.out.println("Enter a search term:");
+            String searchTerm = scan.nextLine();
 
-                // Prompts user to enter string search term
-                System.out.println("Enter a search term:");
-                String searchTerm = scan.nextLine();
+            // Creating Search Object that will return JSON String Body
+            searchResponse = SearchConnect.searchTitle(searchTerm);
 
-                // Creating Search Object that will return JSON String Body
-                searchResponse = SearchConnect.searchTitle(searchTerm);
+            // Json String Body from the HTTP response
+            results = searchResponse.body();
+        }
 
-                // Json String Body from the HTTP response
-                results = searchResponse.body();
+        if (userInput == 2) {
 
-                // Breaks while loop and exits Input class
-                break;
-            }
-            
-            if (userInput == 2) {
+            // Prompts user to enter string search term
+            System.out.println("Enter the first ingredient:");
+            String ingredientOne = scan.nextLine();
 
-                // Prompts user to enter string search term
-                System.out.println("Enter the first ingredient:");
-                String ingredientOne = scan.nextLine();
+            boolean addIngredientValid = false;
+            while(!addIngredientValid) {
 
-                // Working on logic that will allow up to 3 ingredients
                 System.out.println("Would you like to add another ingredient?");
                 System.out.println("y for Yes, n for No:");
                 secondItem = scan.nextLine().toLowerCase();
 
                 if (validChoiceStr.contains(secondItem) != true) {
                     System.out.println("Invalid choice, please try again.");
-                    Input.term();
+                    continue;
+                }else{
+                    addIngredientValid = true;
                 }
+            }
 
-                if (secondItem.equals("n")) {
-                    String ingredientTwo = "";
-                    String ingredientThree = "";
-                    searchResponse = SearchConnect.searchIngredients(ingredientOne, ingredientTwo, ingredientThree);
-                    results = searchResponse.body();
-                    break;
+            if (secondItem.equals("n")) {
+                String ingredientTwo = "";
+                String ingredientThree = "";
+                searchResponse = SearchConnect.searchIngredients(ingredientOne, ingredientTwo, ingredientThree);
+                results = searchResponse.body();
 
-                } else if (secondItem.equals("y")) {
-                    System.out.println("Enter the second ingredient:");
-                    String ingredientTwo = scan.nextLine();
+
+            } else if (secondItem.equals("y")) {
+                System.out.println("Enter the second ingredient:");
+                String ingredientTwo = scan.nextLine();
+
+                addIngredientValid = false;
+                while(!addIngredientValid) {
+
                     System.out.println("Would you like to a final ingredient?");
                     System.out.println("y for Yes, n for No:");
                     thirdItem = scan.nextLine().toLowerCase();
 
                     if (validChoiceStr.contains(thirdItem) != true) {
                         System.out.println("Invalid choice, please try again.");
-                        Input.term();
-                    }
-
-                    if (thirdItem.equals("n") ) {
-                        String ingredientThree = "";
-                        searchResponse = SearchConnect.searchIngredients(ingredientOne, ingredientTwo, ingredientThree);
-                        results = searchResponse.body();
-                        break;
-
-                    } else if (thirdItem.equals("y")) {
-
-                        System.out.println("Enter the last ingredient:");
-                        String ingredientThree = scan.nextLine();
-
-                        // Creating Search Object that will return JSON String Body
-                        searchResponse = SearchConnect.searchIngredients(ingredientOne, ingredientTwo, ingredientThree);
-
-                        // Json String Body from the HTTP response
-                        results = searchResponse.body();
-                        break;
+                        continue;
+                    }else{
+                        addIngredientValid = true;
                     }
                 }
 
-            }
+                if (thirdItem.equals("n") ) {
+                    String ingredientThree = "";
+                    searchResponse = SearchConnect.searchIngredients(ingredientOne, ingredientTwo, ingredientThree);
+                    results = searchResponse.body();
 
-            break;
+
+                } else if (thirdItem.equals("y")) {
+
+                    System.out.println("Enter the last ingredient:");
+                    String ingredientThree = scan.nextLine();
+
+                    // Creating Search Object that will return JSON String Body
+                    searchResponse = SearchConnect.searchIngredients(ingredientOne, ingredientTwo, ingredientThree);
+
+                    // Json String Body from the HTTP response
+                    results = searchResponse.body();
+
+                }
+            }
 
         }
 
